948c1255f62f546da4fcb9de11da6903e9ae170f
Jgg023 <mightyfresh1@gmail.com>
2023-01-18 21:44:57 -0500

    fixed server

--- server.js
+++ server.js
@@ -33,9 +33,5 @@ app.get("/notes", function (req, res) {
   });
 
   app.listen(port, function () {
-    console.log(`Server listening on port ${port}. );
+    console.log(`Server listening on port ${port}.` );
   }); 
-
-  
-
-
